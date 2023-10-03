d96ad5c1eedd6230ad43367f88cc37135258d7c1
Pawel <pawelkarabowicz@gmail.com>
2023-10-03 12:50:14 +0200

    Initial Commit

---
+++ aspp/asppApp.swift
@@ -0,0 +1,17 @@
+//
+//  asppApp.swift
+//  aspp
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct asppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
